1b3145a0663c419f63d1088e3d9fe4ef60403a3d
k1894526 <bohyun.kim@kcl.ac.uk>
2022-11-02 17:18:16 +0900

    Added Pagination

--- main.py
+++ main.py
@@ -1,45 +1,67 @@
 from requests import get
 from bs4 import BeautifulSoup
 
-# 오늘 출력하는 거까지 끝냈고 내일 페이징 기능 추가
-def extract_wwr_jobs(keyword):
+def get_pages(keyword):
   base_url = "https://www.jobkorea.co.kr/Search/?stext="  
   response = get(f"{base_url}{keyword}")
+
+  soup = BeautifulSoup(response.text, 'html.parser')
+  pagination = soup.find('div', class_="tplPagination newVer wide")
+  page = pagination.find_all('li')
+
+  count_pages = len(page)
+
+  if count_pages == 0:
+    return 1
+  elif count_pages >= 10:
+    return 10
+  else:
+    return count_pages
+
+
+def extract_wwr_jobs(keyword):
+  base_url = ("https://www.jobkorea.co.kr/Search/?stext=")
   
-  if not response.status_code == 200:
+  if not get(base_url).status_code == 200:
     print ("Can't request website")
   else:
     results = []
+    pages = get_pages(keyword)
+    print (f"Found {pages} pages")
 
-    soup = BeautifulSoup(response.text, "html.parser")
-    
-    recruit_info = soup.find_all('div', class_="recruit-info")
-    for list in recruit_info:
-      lists = list.find_all('li', class_="list-post")
-      for l in lists:
-        anchor = l.find('a')
-        company = anchor['title']
-        link = anchor['href']
-        exp = l.find('span', class_="exp")
-        edu = l.find('span', class_="edu")
-        loc = l.find('span', class_="loc long")
+    for pg in range(pages):
+      response = get(f"{base_url}{keyword}&Page_No={pg + 1}")
+      soup = BeautifulSoup(response.text, "html.parser")
+      
+      recruit_info = soup.find_all('div', class_="recruit-info")
+      for list in recruit_info:
+        lists = list.find_all('li', class_="list-post")
+        for l in lists:
+          anchor = l.find('a')
+          company = anchor['title']
+          link = anchor['href']
+          
+          exp = l.find('span', class_="exp")
+          edu = l.find('span', class_="edu")
+          loc = l.find('span', class_="loc long")
 
-        job_data = {
-          'link' : f"https://www.jobkorea.co.kr{link}",
-          'company' : company,
-          'experience' : exp.string,
-          'education' : edu.string,
-          'location' : loc.string
-        }
+          if edu != None:
 
-        results.append(job_data)
+            job_data = {
+              'link' : f"https://www.jobkorea.co.kr{link}",
+              'company' : company,
+              'experience' : exp.string,
+              'education' : edu.string,
+              'location' : loc.string
+            }
+            results.append(job_data)
 
     for result in results:
       print (result)
       print ("\n//////\n")
- 
 
+    num_of_results = len(results)
+    print (f"{num_of_results} results in total")
 
 extract_wwr_jobs("developer")
-
 
